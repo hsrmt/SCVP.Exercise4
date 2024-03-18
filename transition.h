60cc431115a9de80e64ee5de09130d46e9b6c307
hatice seremet <haticeesrmt52@gmail.com>
2024-03-18 14:58:07 -0400

    building hierarchical PNs

--- transition.h
+++ transition.h
@@ -7,14 +7,21 @@
 
 // Transition:
 // TODO
-template<unsigned int N = 1, unsigned int M = 1>
+template<unsigned int N = 1, unsigned int M = 1, unsigned int L = 0>
 SC_MODULE(transition){
     sc_port<placeInterface<>, N, SC_ALL_BOUND> in;
     sc_port<placeInterface<>, M, SC_ALL_BOUND> out;
+    sc_port<placeInterface<>, L, SC_ZERO_OR_MORE_BOUND> inhibitors;  // Inhibitor places
 
     SC_CTOR(transition){}
 
     void fire(){
+        for (int i = 0; i < L; ++i) {
+            if (inhibitors[i]->testTokens() != 0) {
+                std::cout << this->name() << ": NOT Fired (Inhibited)" << std::endl;
+                return;  // Inhibited by the presence of tokens in an inhibitor place
+            }
+        }
         for (int i = 0; i < N; ++i) {
             if (!in[i]->testTokens()) {
                 std::cout << this->name() << ": NOT Fired" << std::endl;
